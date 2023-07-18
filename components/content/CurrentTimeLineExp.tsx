f5a8593025f1236b77edb4a75feb390605e1c3a5
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-07-19 00:24:00 +0530

    port-prs

---
+++ components/content/CurrentTimeLineExp.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Timeline, TimelineEvent } from './TimeLineExp';
+
+const CurrentTimeLineExp = () => {
+  return (
+    <Timeline>
+
+      <TimelineEvent active>
+
+        <TimelineEvent.Title><a href='https://www.ia.com.mx' target='_blank' rel='noopener noreferrer' className='hover:text-white hover:underline transition-all ease'>IA Interactive</a> | mar. 2023 - Currently</TimelineEvent.Title>
+
+        <TimelineEvent.Description>
+          Frontend development and maintenance of projects implementing agile methodologies (Kanban in Jira) and best practices such as SOLID principles, pixel perfect, clean architecture, clean code and using microfrontends and storybook practices to facilitate the use of reusable components using the following technologies and tools: Jira, SCRUM, Git, Github, Bitbucket, HTML, CSS, SASS, Tailwind, JavaScript, TypeScript, React, Next.js, Redux, Jest, Storybook, GitFlow and Microfrontends concepts and practices.
+        </TimelineEvent.Description>
+
+      </TimelineEvent>
+
+      <TimelineEvent>
+
+        <TimelineEvent.Title>Freelance | feb. 2022 - mar. 2023</TimelineEvent.Title>
+
+        <TimelineEvent.Description>
+          Design and complete development of web pages/applications in a freelancer way applying agile methodologies, clean architecture, SOLID principles and pixel perfect in the projects I have done, I have developed almost all types of projects from Landing pages, Manageable stores with registration and login, Manageable blogs, dashboards, web pages with a variety of sections and more from prototyping, design using Balsamiq Wireframes, Adobe Photoshop and Figma, also from the design to the programming and migration.
+        </TimelineEvent.Description>
+
+      </TimelineEvent>
+
+      <TimelineEvent last>
+
+        <TimelineEvent.Title>Studio Iluxion | may. 2022 - jul. 2022</TimelineEvent.Title>
+
+        <TimelineEvent.Description>
+          Web designer and responsive web designer and frontend programmer on projects using Figma, HTML, CSS, SCSS, SASS, Bootstrap, JavaScript, jQuery implementing SCRUM, pixel perfect and clean code.
+        </TimelineEvent.Description>
+
+      </TimelineEvent>
+
+    </Timeline>
+  )
+}
+
+export default CurrentTimeLineExp;
