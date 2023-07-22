bab79784e919684e5fafe87b82b5d6f078879adf
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-07-22 22:35:31 +0530

    web-pc

--- components/content/CurrentFavTech.tsx
+++ components/content/CurrentFavTech.tsx
@@ -3,14 +3,12 @@ import AnimationContainer from '../utils/AnimationContainer';
 
 const CurrentFavTech = () => {
   return (
-    <AnimationContainer customClassName='flex flex-col justify-center items-center lg:items-start mb-5 mx-auto lg:mx-0'>
-      <div className='flex flex-col justify-center items-center lg:items-start'>
-
+    <AnimationContainer customClassName="flex flex-col justify-center items-center lg:items-start mb-5 mx-auto lg:mx-0">
+      <div className="flex flex-col justify-center items-center lg:items-start">
         <AllFavTechs />
-
       </div>
     </AnimationContainer>
-  )
-}
+  );
+};
 
 export default CurrentFavTech;
