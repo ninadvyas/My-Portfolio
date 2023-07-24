8049ab8cea13c3b27694f01357c43f5435d82bae
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-07-24 20:14:57 +0530

    added-exp

--- app/(user)/blog/page.tsx
+++ app/(user)/blog/page.tsx
@@ -1,11 +1,12 @@
 import SectionContainer from "@/components/utils/SectionContainer";
+
 import TitleSectionPageContainer from "@/components/utils/TitleSectionPageContainer";
 
 const Blog = () => (
   <SectionContainer>
 
     <div className='w-full flex flex-col items-center gap-6' style={{ height: '100vh' }}>
-
+   
       <TitleSectionPageContainer title='Coming soon...' />
 
     </div>
