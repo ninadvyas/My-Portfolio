6e075b55557bae4d902d3402f601a529b43c8aad
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-07-26 01:37:52 +0530

    header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -8,12 +8,17 @@ const Header = () => {
     <HeaderAnimation>
       <nav className='w-full lg:min-w-xs lg:max-w-screen-md flex items-center justify-between flex-row relative border-gray-700 px-5 py-8 sm:pb-8 bg-opacity-60 text-gray-100 gap-5 lg:gap-0'>
 
-        <div>
+        <div >
           <h1>
             <Link href='/'>
-              <strong className='hover:text-blue-500'>Ninad</strong>
+              <strong className='hover:text-green-500 '>Ninad</strong>
             </Link>
           </h1>
+          {/* <h1 className=' rounded-md text-center '>
+            <Link href='https://blogbyninad.vercel.app'  target='_blank' rel='noreferrer' className='text-center'>
+              <strong className='text-white hover:text-blue-500 cursor-progress text-xl'>Blogs</strong>
+            </Link>
+          </h1> */}
         </div>
 
         <div className='ml-[-0.80rem]'>
