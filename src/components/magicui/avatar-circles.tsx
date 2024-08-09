4a34ea46a556046bf5f5d257d639bfba078ad334
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2024-08-09 11:45:22 +0530

    fix: duplicates elements

--- src/components/magicui/avatar-circles.tsx
+++ src/components/magicui/avatar-circles.tsx
@@ -19,8 +19,6 @@ const AvatarCircles = ({
     <div className={cn("z-10 flex -space-x-4 rtl:space-x-reverse", className)}>
       {avatarUrls.map((url, index) => (
         <Image
-          width='10'
-          height='10'
           key={index}
           className="h-10 w-10 rounded-full border-2 border-white dark:border-gray-800 bg-black"
           src={url}
